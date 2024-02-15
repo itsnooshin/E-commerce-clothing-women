f6c891ad0be8f9caaf931899185580aabe54efa2
Nooshin bakhtiari <nooshindev@gmail.com>
2024-02-15 16:18:18 +0330

    add section called  sustainability for show more about This shop

---
+++ components/Sustainability.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+import Header from '../public/Sustainability.png';
+import { Box, Button, Typography } from '@mui/material';
+
+function Sustainability() {
+  return (
+    <Box
+      sx={{ position: 'relative', width: '100%', height: '300px', mt: '40px' }}
+    >
+      <Image
+        alt="this is a header"
+        src={Header}
+        width={500}
+        height={500}
+        style={{
+          objectFit: 'cover',
+          width: '100%',
+        }}
+      />
+
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'flex-end',
+          justifyContent: 'flex-end ',
+          flexDirection: 'column',
+          position: 'absolute',
+          top: '120%',
+          right: '50px',
+        }}
+      >
+        <Typography maxWidth="450px">
+          Stylish sustainability in clothing promotes eco-friendly choices for a
+          greater future
+        </Typography>
+        <Button
+          sx={{
+            textTransform: 'none',
+            color: '#000000',
+            backgroundColor: '#fff',
+          }}
+        >
+          Sustainability
+        </Button>
+      </Box>
+    </Box>
+  );
+}
+
+export default Sustainability;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import BestSellers from '@/components/BestSellers';
 import BannerHeader from '@/components/BannerHeader';
 import Collection from '@/components/Collection';
 import MoodiWeek from '@/components/MoodiWeek';
+import Sustainability from '@/components/Sustainability';
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <BestSellers />
       <Collection />
       <MoodiWeek />
+      <Sustainability />
     </>
   );
 }
