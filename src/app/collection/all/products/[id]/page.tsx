802bb79119cd6eb091698c8d3c585fa37ba33085
itsnooshin <nooshindev@gmail.com>
2024-05-01 18:36:12 +0330

    feat: update the product detail

--- src/app/collection/all/products/[id]/page.tsx
+++ src/app/collection/all/products/[id]/page.tsx
@@ -25,5 +25,5 @@ export default function page({ params }: any) {
 
   if (!singleProduct) return <SpinnerLoader />;
 
-  return <ProductDetail product={singleProduct} />;
+  return <ProductDetail product={singleProduct} link={'all'} />;
 }
